b8822599f5c6cf45e2c730513efbd20bc42af817
Blade Boles <bladerboles@gmail.com>
2019-12-10 13:01:13 -0700

    It works!

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import shoppingList from './scripts/shopping-list';
+import $ from 'jquery';
+
+const main = function () {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+};
+
+$(main);
